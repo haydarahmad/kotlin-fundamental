fb2dd21d881398d013edd1cea6135e48c8a882cd
haydarahmad <fattanhay0@gmail.com>
2022-08-19 21:08:32 +0700

    DataTypeVariable.kt

--- app/src/main/java/com/haydar/logickotlin/kotlinfundamental/DataTypeVariable.kt
+++ app/src/main/java/com/haydar/logickotlin/kotlinfundamental/DataTypeVariable.kt
@@ -3,8 +3,43 @@ package com.haydar.logickotlin.kotlinfundamental
 // val immutable
 // var muttable
 
+/*
+   pada umumnya variable digunakan untuk menyimpan informasi atau nilai
+   yang akan dikelola di dalam sebuah program
+ */
+
+
+/*
+   variable membutuhkan kata kunci var atau val, identifier,type,dan initialization
+ */
+// var identifier: Type = initalization
+
+// contoh tipe string
+//var company:String = "haydar"
+
+/* identifier
+   merupakan sebuah nama dari variable
+ */
+
+/* type
+    tipe data dari variabel
+    karena kotlin memakai type inference jadi tidak perlu
+ */
+
+/* initialization
+   merupakan nilai awal dari sebuah variabel
+ */
+
+// contoh mengunakan operator (+) pada tipe string
+//fun main() {
+//    val firstWord = "Dicoding"
+//    val lastword = "Academy"
+//    print(firstWord + lastword)
+//}
+
+// contoh mengunakan operator (+) pada tipe int
 fun main() {
-    val a = 2
-    val b = 4
-    println(a + b)
+    val valueA: Int = 10
+    val valueB = 20
+    print(valueA + valueB)
 }
