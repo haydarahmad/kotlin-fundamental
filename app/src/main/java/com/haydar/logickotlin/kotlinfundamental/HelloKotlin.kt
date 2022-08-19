2dc1ace5ed2f5d9d84add1ca09e7bf64fb7d4199
haydarahmad <fattanhay0@gmail.com>
2022-08-19 20:49:01 +0700

    HelloKotlin.kt

---
+++ app/src/main/java/com/haydar/logickotlin/kotlinfundamental/HelloKotlin.kt
@@ -0,0 +1,15 @@
+package com.haydar.logickotlin.kotlinfundamental
+
+// main function
+
+//fun main() {
+//    println("hello world")
+//}
+
+fun main() {
+    val name = "haydar"
+
+    print("Hello my name is ")
+    println(name)
+    print(if (true)"always true" else "always false")
+}
