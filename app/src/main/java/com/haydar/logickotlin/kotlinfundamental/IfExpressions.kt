407f52c8f2c5a4b0562a0deaf735c2e587075283
haydarahmad <fattanhay0@gmail.com>
2022-08-20 21:37:29 +0700

    IfExpressions.kt(belum selesai)

---
+++ app/src/main/java/com/haydar/logickotlin/kotlinfundamental/IfExpressions.kt
@@ -0,0 +1,68 @@
+package com.haydar.logickotlin.kotlinfundamental
+
+/* if expression
+  Saat mengembangkan sebuah program, kita pasti bertemu dengan
+  alur program yang perlu sebuah kondisi untuk menjalankan sebuah
+  statement atau expression.
+
+  if expression mengunakan kata kunci (if)
+  if digunakan ketika kita ingin menguji suatu kondisi
+  dalam menjalankan proses
+
+  If akan mengeksekusi sebuah statement atau expression jika
+  hasil evaluasi dari expressions yang diberikan pada blok if bernilai true.
+  Sebaliknya, jika bernilai false maka proses yang ditentukan akan dilewatkan.
+
+  contohnya
+fun main() {
+    val openHours = 7
+    val now = 20
+
+    if (now > openHours){
+        println("office alredy open")
+    }
+}
+
+ jika kita ingin melakukan seleksi atau jika ingin menambahkan opsi kita harus
+ menggukan (else)
+
+ contohnya
+ fun main() {
+    val openHours = 7
+    val now = 20
+    val office:String
+
+    if (now > openHours){
+        office = "office alredy open"
+    }else {
+        office = "office is closed"
+    }
+
+    print(office)
+}
+
+ else akan dijalankan ketika kondisinya false
+
+ If merupakan sebuah expressions yang dapat mengembalikan nilai,
+ sehingga kita dapat menyimpan hasilnya ke dalam sebuah variabel
+
+ contohnya
+fun main() {
+    val openHours = 7
+    val now = 20
+    val office: String
+
+    office = if (now > openHours ){
+        "office alredy open"
+    } else{
+        "office is closed"
+    }
+
+    print(office)
+
+}
+ */
+
+
+
+
