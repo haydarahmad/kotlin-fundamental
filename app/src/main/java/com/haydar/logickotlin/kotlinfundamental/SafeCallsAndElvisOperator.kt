6253693b38cde84895ad677f82edeeaf75810edf
haydarahmad <fattanhay0@gmail.com>
2022-08-22 20:21:19 +0700

    SafeCallsAndElvisOperator.kt

--- app/src/main/java/com/haydar/logickotlin/kotlinfundamental/SafeCallsAndElvisOperator.kt
+++ app/src/main/java/com/haydar/logickotlin/kotlinfundamental/SafeCallsAndElvisOperator.kt
@@ -27,7 +27,49 @@ package com.haydar.logickotlin.kotlinfundamental
     val textLength = text?.length ?:7
     println(textLength)
 }
+
  */
+
+/* String template
+
+   Sebuah fitur yang memungkinkan kita untuk menyisipkan sebuah variabel ke
+   dalam sebuah String tanpa concatenation (penggabungan objek String menggunakan +)
+
+   Untuk menggunakan string template, kita hanya perlu menambahkan karakter $
+   sebelum nama variabel yang akan disisipkan seperti berikut
+
+   fun main() {
+    val name = "kotlin"
+    print("my name is " + $name)
+
+}
+
+
+   Variabel yang dapat disisipkan tidak sebatas String. Kita juga bisa
+   menyisipkan objek lain misal Int atau Double seperti berikut
+
+   fun main() {
+    val name = "kotlin"
+    val old = 3
+
+    print("my name is $name, im $old years old")
+}
+
+ kita juga bisa menyisipkan sebuah expression ke dalam sebuah string
+ tamplate
+
+ fun main() {
+    val hour = 7
+    print("office ${if (hour > 7)"alredy close" else"is open"}")
+}
+ */
+
+
+
+
+
+
+
 
 
 
