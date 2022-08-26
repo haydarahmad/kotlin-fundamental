d4443fcc9ea47b30d781e1a796cd3ff1a4d25c86
haydarahmad <fattanhay0@gmail.com>
2022-08-26 13:10:45 +0700

    ExpressionAndStatements.kt

--- app/src/main/java/com/haydar/logickotlin/controlflow/ExpressionAndStatements.kt
+++ app/src/main/java/com/haydar/logickotlin/controlflow/ExpressionAndStatements.kt
@@ -1,5 +1,7 @@
 package com.haydar.logickotlin.controlflow
 
+import java.lang.Integer.sum
+
 /* statment
 
     val openOffice = 7
@@ -33,6 +35,36 @@ package com.haydar.logickotlin.controlflow
         "office already open" else "Office close"
     print(office)
 
-}
+    expression juga bisa terdapat subuah expression lagi
+
+    fun main() {
+    sum(1,1 * 4)
+    }
+
+    fun sum(value1: Int, value2: Int) = value1 + value2
+
+
+
  */
+
+/* perbedaannya
+
+fun main(){
+    val value1 = 10
+    val value2 = 10
+
+    sum(value1,value2)
+}
+
+fun sum (value1:Int,value2: Int) = value1 + value2
+
+pada kode di atas deklarasi variabel value1 dan value2
+merupakan sebuah statement. Sedangkan pemanggilan fungsi
+sum seperti yang dijelaskan di atas, merupakan sebuah expression.
+
+ */
+
+
+
+
 
